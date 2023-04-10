e30f7fc6c74a9efbdb04946398fb6dfcc6fd43ab
Gonzalo Busto Musi <gonzalo.busto@gmail.com>
2023-04-10 22:26:39 +0200

    Added 2 retries in case of timeout. The timeout is lowered to 15 seconds.

--- src/service/caller.rs
+++ src/service/caller.rs
@@ -1,28 +1,53 @@
-use std::time::Duration;
-
-use reqwest::Response;
+use chrono::Local;
+use reqwest::{Error, Response};
 use serde::Serialize;
+use std::fmt::Debug;
+use std::time::Duration;
 
 use crate::utils::constants::lolesports;
 use color_eyre::{eyre::Context, Result};
+use tokio::time::sleep;
 
 pub async fn make_get_request<T>(endpoint: &str, args: Option<&T>) -> Result<Response>
 where
-    T: Serialize,
+    T: Serialize + Debug,
 {
     let client = reqwest::Client::builder()
-        .timeout(Duration::from_secs(30))
+        .timeout(Duration::from_secs(15))
         .build()?;
 
-    let mut b = client
-        .get(format!("{}{}", lolesports::BASE_URL, endpoint))
-        .header("x-api-key", "0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z");
+    let mut attempts = 2;
+    let retry_duration = Duration::from_secs(5);
 
-    if let Some(arguments) = args {
-        b = b.query(arguments);
+    loop {
+        let mut b = client
+            .get(format!("{}{}", lolesports::BASE_URL, endpoint))
+            .header("x-api-key", "0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z");
+
+        if let Some(arguments) = args {
+            b = b.query(arguments);
+        }
+
+        let result = b.send().await;
+
+        match result {
+            Ok(response) => return Ok(response),
+            Err(e) => {
+                if e.is_timeout() && attempts > 0 {
+                    attempts -= 1;
+                    println!(
+                        "{} - Request to {} with args {:?} timed out ",
+                        Local::now().format("%Y-%m-%d %H:%M:%S.%f"),
+                        &endpoint,
+                        args
+                    );
+
+                    sleep(retry_duration).await;
+                } else {
+                    return Err(e)
+                        .with_context(|| format!("Failed to request data from the LoLEsports API:{endpoint:?} with args -> {args:?}"));
+                }
+            }
+        }
     }
-
-    b.send()
-        .await
-        .with_context(|| format!("Failed to request data from the LoLEsports API:{endpoint:?}"))
 }
